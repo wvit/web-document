2bb0c4a0badd198a2c7b68dee95442c9018920ab
wvit <1999wuwei@gmail.com>
2024-04-08 19:19:28 +0800

    调整vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,25 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const { entry } = process.env
+
+const allEntry = {
+  background: {
+    'background/index': 'src/background/index.ts',
+  },
+  action: {
+    'action/index.html': 'src/action/index.html',
+  },
+  page: {
+    'page/index.html': 'src/page/index.html',
+  },
+}
+
+/** 自定义当前需要构建哪些入口，减少开发中 build 时间 */
+const input = (entry || Object.keys(allEntry).join(','))
+  .split(',')
+  .reduce((prev, key) => ({ ...prev, ...allEntry[key] }), {})
+
 export default defineConfig({
   root: 'src',
 
@@ -17,11 +36,7 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
 
     rollupOptions: {
-      input: {
-        'background/index': 'src/background/index.ts',
-        'action/index.html': 'src/action/index.html',
-        'page/index.html': 'src/page/index.html',
-      },
+      input,
       output: {
         entryFileNames: `[name].js`,
       },
