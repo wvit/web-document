deec7d015c4a8834c92c2ac57286852ba4179f83
wvit <1999wuwei@gmail.com>
2024-04-22 21:18:48 +0800

    update @vorker/utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
 export * from '@vorker/utils'
 export * from './chrome'
 export * from './message'
-export * from './tools'
--- src/utils/tools.ts
+++
@@ -1,8 +0,0 @@
-const textEncoder = new TextEncoder()
-
-/** 获取内容占用磁盘空间大小 */
-export const getStorageSize = content => {
-  const dataEncode = textEncoder.encode(content)
-  const storageSize = Number((dataEncode.length / 1024 / 1024).toFixed(2))
-  return storageSize
-}
